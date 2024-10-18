af4a7611d49581a686bdc17551947e4297100909
Kieran Mai <kieran.noel@icloud.com>
2024-10-18 04:00:33 +0200

    add dialog

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltipModule, TooltipPosition } from '@angular/material/tooltip';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
@@ -12,4 +14,9 @@ import { MatTooltipModule, TooltipPosition } from '@angular/material/tooltip';
 })
 export class UserComponent {
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+
+  constructor(public dialog: MatDialog) {}
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
