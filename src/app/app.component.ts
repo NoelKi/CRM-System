5f8ed010139df508e7ae546303c5137085e9a381
Amadeus Scherkenbach <scherkenbach@outlook.de>
2024-10-18 02:22:45 +0200

    Refactor app component to use dynamic menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { menuItems } from './menuItem';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule, MatListModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'CRM-System';
 
+  menuItems = signal(menuItems);
 
+  collapsed = signal(false);
 }
---
+++ src/app/menuItem.ts
@@ -0,0 +1,36 @@
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route?: string;
+  subItems?: MenuItem[];
+};
+
+export const menuItems: MenuItem[] = [
+  {
+    icon: 'dashboard',
+    label: 'Dashboard',
+    route: '/dashboard'
+  },
+  {
+    icon: 'shopping_cart',
+    label: 'Orders',
+    route: '/orders'
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+    subItems: [
+      {
+        icon: 'person',
+        label: 'Profile',
+        route: '/profile'
+      },
+      {
+
+        icon: 'lock',
+        label: 'Change Password',
+        route: '/change-password'
+      }
+    ]
+  }
+];
