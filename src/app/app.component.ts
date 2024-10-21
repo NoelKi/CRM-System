f45ec1123960a4daaffe4c539a56d4b1bc582535
Amadeus Scherkenbach <scherkenbach@outlook.de>
2024-10-21 13:41:57 +0200

    [app.component] remove unnessary imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
 import { menuItems } from './menuItem';
 @Component({
   selector: 'app-root',
@@ -27,5 +25,4 @@ export class AppComponent {
   title = 'CRM-System';
 
   menuItems = signal(menuItems);
-
 }
