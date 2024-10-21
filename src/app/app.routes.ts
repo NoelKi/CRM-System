5bd5eeb8ce559ba85b5eb6a54b8e4b764d189a07
Kieran Mai <kieran.noel@icloud.com>
2024-10-21 16:02:39 +0200

    add detailed user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'user', component: UserComponent },
+  { path: 'user/:id', component: UserDetailComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'settings', component: SettingsComponent }
 ];
