1e70ac5d459bafb59e635d44d549bfc2c2389678
Amadeus Scherkenbach <scherkenbach@outlook.de>
2024-10-21 13:44:19 +0200

    formatted

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,28 +1,24 @@
 import { Injectable, signal } from '@angular/core';
-import { User } from '../../models/user.model';
 import { users } from '../../fake-db/user.data';
+import { User } from '../../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class UserService {
   users = signal(users);
 
-  constructor() {
-  }
+  constructor() {}
 
   addUser(user: User) {
     // this.users().push(user);
     const users = this.users();
     users.push(user);
     this.users.set(users);
-    // console.log(this.users);
+    // console.log(this.users());
   }
 
   deleteUser(user: User) {
-    this.users().splice(
-      this.users().indexOf(user), 1
-    )
+    this.users().splice(this.users().indexOf(user), 1);
   }
 }
