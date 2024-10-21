d8ca222360ef3b9848f6bcc9a1e9d3666b2b895b
Kieran Mai <kieran.noel@icloud.com>
2024-10-21 19:23:02 +0200

    after merge

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,13 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../models/user.model';
+import { DialogEditAdressComponent } from '../dialog-edit-adress/dialog-edit-adress.component';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
@@ -19,18 +24,23 @@ export class UserDetailComponent implements OnInit {
   private _route = inject(ActivatedRoute);
   private _userService = inject(UserService);
 
-  // constructor(private route: ActivatedRoute) {}
+  // constructor(private route: ActivatedRoute, public dialog: MatDialog) {}
   constructor() {}
 
   ngOnInit(): void {
-    // this.route.paramMap.subscribe((paramMap) => {
-    //   this.userId = paramMap.get('id');
-    //   console.log('Got the ID: ' + this.userId);
-    // });
-    const paramId = this._route.snapshot.paramMap.get('id');
-    if (paramId === null) {
-      return;
-    }
-    this.user = this._userService.getUser(paramId);
+    this.route.paramMap.subscribe((paramMap) => {
+      this.userId = paramMap.get('id');
+      console.log('Got the ID: ' + this.userId);
+    });
+  }
+
+  openAdressDialog() {}
+
+  editUserAdress() {
+    const dialog = this.dialog.open(DialogEditAdressComponent);
+    dialog.componentInstance.user = this.user;
+  }
+  editUserDetail() {
+    this.dialog.open(DialogEditUserComponent);
   }
 }
