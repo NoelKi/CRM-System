9d00fce03091c01eeec68f8939b8ab1e361718ff
Kieran Mai <kieran.noel@icloud.com>
2024-10-28 17:02:37 +0100

    add icon for issue #5

--- src/app/user-detail/components/profil-picture/profil-picture.component.ts
+++ src/app/user-detail/components/profil-picture/profil-picture.component.ts
@@ -1,12 +1,30 @@
 import { Component, input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profil-picture',
   standalone: true,
-  imports: [],
-  template: `<img [src]="source()" />`,
+  imports: [MatIcon],
+  template: `<img
+      [src]="source()"
+      alt="profil-image"
+      (mouseenter)="onHover()"
+      (mouseleave)="onLeave()"
+    />
+    <mat-icon aria-hidden="false" [class]="{ hovered: isHovered }" aria-label="edit"
+      >edit</mat-icon
+    >`,
   styleUrl: './profil-picture.component.scss'
 })
 export class ProfilPictureComponent {
   source = input.required<string>();
+  isHovered = false;
+
+  onHover() {
+    this.isHovered = true;
+  }
+
+  onLeave() {
+    this.isHovered = false;
+  }
 }
