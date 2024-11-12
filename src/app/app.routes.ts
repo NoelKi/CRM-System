212d4422919d084cd4861181b33215029e3ab212
Kieran Mai <kieran.noel@icloud.com>
2024-11-12 10:26:13 +0100

    add playground route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PlaygroundComponent } from './playground/playground/playground.component';
 import { SettingsComponent } from './settings/settings.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserComponent } from './user/user.component';
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'user/:id', component: UserDetailComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'playground', component: PlaygroundComponent },
   { path: 'settings', component: SettingsComponent }
 ];
