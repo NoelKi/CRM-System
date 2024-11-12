6121a73266bf673444dda261d641103099dbf69b
Kieran Mai <kieran.noel@icloud.com>
2024-11-12 16:00:29 +0100

    hide user-signal

---
+++ src/app/user-signal/user-signal.component.ts
@@ -0,0 +1,190 @@
+import { CdkDragDrop, CdkDropList, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { DatePipe } from '@angular/common';
+import { Component, inject, OnInit, viewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Router, RouterModule } from '@angular/router';
+import { BehaviorSubject, combineLatest, map, switchMap } from 'rxjs';
+import { UserService } from '../services/user.service';
+import { DialogAddUserComponent } from '../user/components/dialog-add-user/dialog-add-user.component';
+import { DialogDeleteUserComponent } from '../user/components/dialog-delete-user/dialog-delete-user.component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [
+    MatIcon,
+    MatButtonModule,
+    MatTooltipModule,
+    MatTableModule,
+    RouterModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    DatePipe,
+    CdkDropList,
+    DragDropModule,
+    MatTableModule
+  ],
+  templateUrl: './user-signal.component.html',
+  styleUrl: './user-signal.component.scss'
+})
+export class UserSignalComponent implements OnInit {
+  private _userService = inject(UserService);
+  private _dialog = inject(MatDialog);
+  private _snackBar = inject(MatSnackBar);
+  private _router = inject(Router);
+
+  private _paginator = viewChild.required(MatPaginator);
+
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'birthDate', 'adress', 'edit'];
+  isLoadingResults = true;
+
+  // BehaviorSubjects
+  private _refreshUsers$ = new BehaviorSubject<void>(undefined);
+  private _sortDirection$ = new BehaviorSubject<string>('asc');
+  private _sortActive$ = new BehaviorSubject<string>('');
+  private _pageIndex$ = new BehaviorSubject<number>(0);
+  private _pageSize$ = new BehaviorSubject<number>(5);
+  private _filter$ = new BehaviorSubject<string>('');
+
+  filterVariables: IGetUsersBehaviorSParams = {
+    pageSize: this._pageSize$,
+    pageIndex: this._pageIndex$,
+    filterValue: this._filter$,
+    sortField: this._sortActive$,
+    sortDirection: this._sortDirection$
+  };
+
+  ngOnInit(): void {
+    this._paginator().pageSize = this._pageSize$.value;
+    console.log(this._filter$);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.displayedColumns, event.previousIndex, event.currentIndex);
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, { duration: 3000 });
+  }
+
+  onFilterChange(value: string) {
+    this._pageIndex$.next(0);
+    this._filter$.next(value);
+  }
+
+  onPageChange(event: PageEvent) {
+    this._pageSize$.next(event.pageSize);
+    this._pageIndex$.next(event.pageIndex);
+  }
+
+  onSortChange(sortState: Sort) {
+    this._pageIndex$.next(0);
+    this._sortDirection$.next(sortState.direction);
+    this._sortActive$.next(sortState.active);
+  }
+
+  usersData$ = combineLatest([
+    this.filterVariables.filterValue,
+    this.filterVariables.sortDirection,
+    this.filterVariables.sortField,
+    this.filterVariables.pageIndex,
+    this.filterVariables.pageSize,
+    this._refreshUsers$
+  ]).pipe(
+    switchMap(([filterValue, sortDirection, sortField, pageIndex, pageSize]) => {
+      this.isLoadingResults = true;
+      return this._userService
+        .getUsersTest({
+          filterValue,
+          sortDirection,
+          sortField,
+          pageIndex,
+          pageSize
+        })
+        .pipe(
+          map((res) => {
+            this.isLoadingResults = false;
+            this._paginator().pageIndex = this._pageIndex$.value;
+            this._paginator().length = res.totalLength;
+            return res.users;
+          })
+        );
+    })
+  );
+
+  openDialog() {
+    const dialogRef = this._dialog.open(DialogAddUserComponent, {
+      height: '660px',
+      width: '620px',
+      maxWidth: '100%'
+    });
+    dialogRef.afterClosed().subscribe((user) => {
+      this.isLoadingResults = true;
+      this._userService.addUser(user).subscribe({
+        next: (res) => {
+          if (res.status === 'OK') {
+            user.profilPicSrc = res.profilePicSrc;
+            user._id = res._id;
+            this.openSnackBar(
+              user.firstName + ' ' + user.lastName + ' was added to Users',
+              'close'
+            );
+            this._router.navigate(['./user/' + res._id]);
+          }
+        },
+        error: (error) => {
+          console.error('Error posting user', error);
+        }
+      });
+    });
+  }
+
+  openDeleteDialog(_id: string, name: string) {
+    const dialogRef = this._dialog.open(DialogDeleteUserComponent, {
+      height: '200px',
+      width: '200px',
+      maxWidth: '100%',
+      data: {
+        name: name
+      }
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === true) {
+        this.isLoadingResults = true;
+        this._userService.deleteUserTest(_id).subscribe({
+          next: (res) => {
+            if (res.status === 'OK') {
+              this._refreshUsers$.next();
+              this.openSnackBar('User succesfully deleted!', 'close');
+            }
+          },
+          error: (error) => {
+            console.error('Error deleting user', error);
+            this.openSnackBar('User was not deleted properly', 'close');
+          }
+        });
+      }
+    });
+  }
+}
+
+interface IGetUsersBehaviorSParams {
+  pageSize: BehaviorSubject<number>;
+  pageIndex: BehaviorSubject<number>;
+  filterValue: BehaviorSubject<string>;
+  sortField: BehaviorSubject<string>;
+  sortDirection: BehaviorSubject<string>;
+}
